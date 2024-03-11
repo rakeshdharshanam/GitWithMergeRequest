886524dba33ee626a3a28e27b4d301dce0009683
rakesh <rakesh.dharshanam333@gmail.com>
2024-03-11 11:55:44 +0530

    added space in return message

--- Gitdemo/src/main/java/com/example/Gitdemo/RestController/UsersRestController.java
+++ Gitdemo/src/main/java/com/example/Gitdemo/RestController/UsersRestController.java
@@ -2,13 +2,19 @@ package com.example.Gitdemo.RestController;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Gitdemo.Configurations.Configurations;
+
 @RestController
 public class UsersRestController {
 
 	private String HELLO = "Hello from rakesh";
 	
 	public String helloMessage() {
-		return HELLO;
+<<<<<<< Updated upstream
+		return HELLO+" "+Configurations.NAME;
+=======
+		return HELLO+Configurations.NAME;
+>>>>>>> Stashed changes
 	}
 	
 }
