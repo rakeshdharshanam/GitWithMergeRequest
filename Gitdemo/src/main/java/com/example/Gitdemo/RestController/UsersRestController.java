b4c1978aa6a6a00c5aa1dfd667e679bf5b640ea6
rakesh <rakesh.dharshanam333@gmail.com>
2024-03-11 12:10:00 +0530

    added the space in return message

--- Gitdemo/src/main/java/com/example/Gitdemo/RestController/UsersRestController.java
+++ Gitdemo/src/main/java/com/example/Gitdemo/RestController/UsersRestController.java
@@ -2,13 +2,17 @@ package com.example.Gitdemo.RestController;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.Gitdemo.Configurations.Configurations;
+
 @RestController
 public class UsersRestController {
 
 	private String HELLO = "Hello from rakesh";
 	
 	public String helloMessage() {
-		return HELLO;
+
+		return HELLO+" "+Configurations.NAME;
+
 	}
 	
 }
